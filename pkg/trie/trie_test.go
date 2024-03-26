b84d91d52befc35d1f4cd4bfa74df2ee8485e360
dasbd72 <twbd723@gmail.com>
2024-03-26 22:32:58 +0800

    test: final state of trie

--- pkg/trie/trie_test.go
+++ pkg/trie/trie_test.go
@@ -9,9 +9,13 @@ func TestTrie(t *testing.T) {
 	type sequence []struct {
 		key, value, hash string
 	}
+	type pairs []struct {
+		key, value string
+	}
 	testCases := []struct {
 		name string
 		seq  sequence
+		want pairs
 	}{
 		{
 			name: "Balance",
@@ -23,6 +27,14 @@ func TestTrie(t *testing.T) {
 				{"a711356", "46", "65e702c5a1fa7d34b1ccfbd130c9a1dc97bff6d83f3237ed06da1a7f801a0754"},
 				{"a711357", "47", "0214f87faeb8417f4e5a73df8ee4aaaf904571fb9f859e2e8aa64f6f003ba3bf"},
 			},
+			want: pairs{
+				{"a711355", "45"},
+				{"a7f9365", "2"},
+				{"a77d337", "1"},
+				{"a77d397", "12"},
+				{"a711356", "46"},
+				{"a711357", "47"},
+			},
 		},
 		{
 			name: "Words",
@@ -31,6 +43,12 @@ func TestTrie(t *testing.T) {
 				{"646f67", "puppy", "779db3986dd4f38416bfde49750ef7b13c6ecb3e2221620bcad9267e94604d36"},
 				{"646f6765", "coin", "ef7b2fe20f5d2c30c46ad4d83c39811bcbf1721aef2e805c0e107947320888b6"},
 				{"686f727365", "stallion", "5991bb8c6514148a29db676a14ac506cd2cd5775ace63c30a4fe457715e9ac84"},
+			},
+			want: pairs{
+				{"646f", "verb"},
+				{"646f67", "puppy"},
+				{"646f6765", "coin"},
+				{"686f727365", "stallion"},
 			},
 		},
 		{
@@ -41,6 +59,13 @@ func TestTrie(t *testing.T) {
 				{"7c303333756d777643cb09c999409608abb642d9", "30", "9562db763cd5d915cc7eed8935e9aeedd33752e2b207c38cecc6cea527bdaee7"},
 				{"7c303333756d777643cb09caaa409608abb642d9", "40", "6aff53fadf211e9b4a452414b7ec0c464b96a23365df6419b8fde90e8a3742fa"},
 				{"111102ad756d76a643cb09cd45409608abb642d9", "50", "b3506d16d769a8aaf5e2fe2f4449a673b408472c04ba0e0837aba0bc9d5364cd"},
+			},
+			want: pairs{
+				{"7c3002ad756d76a643cb09cd45409608abb642d9", "10"},
+				{"7c303333756d555643cb09cd45409608abb642d9", "20"},
+				{"7c303333756d777643cb09c999409608abb642d9", "30"},
+				{"7c303333756d777643cb09caaa409608abb642d9", "40"},
+				{"111102ad756d76a643cb09cd45409608abb642d9", "50"},
 			},
 		},
 	}
@@ -54,6 +79,12 @@ func TestTrie(t *testing.T) {
 				}
 				if got, ok := trie.Get([]byte(s.key)); !ok || string(got) != s.value {
 					t.Errorf("Get(%q) = %q, %t, want %q, true", s.key, got, ok, s.value)
+				}
+			}
+			// Check the final state
+			for _, w := range tc.want {
+				if got, ok := trie.Get([]byte(w.key)); !ok || string(got) != w.value {
+					t.Errorf("Get(%q) = %q, %t, want %q, true", w.key, got, ok, w.value)
 				}
 			}
 		})
